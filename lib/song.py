c8ae870a0c30a87c5db1155246696bc37bcdc6dd
James Hawk <jph94880@gmail.com>
2023-08-02 22:12:51 -0500

    Passes two tests

--- lib/song.py
+++ lib/song.py
@@ -10,19 +10,32 @@
 
 class Song:
     count = 0
+    genres = []
     def __init__(self, name, artist, genre):
         self.name = name 
         self.artist = artist 
         self.genre = genre
         self.add_song_to_count()
+        self.add_to_genres()
     def add_song_to_count(self):
-        # Song objects are initializing. count is not incrementing += 1. 
+        # Yields desired behavior, does not pass test. 
+        print("Before increment:", Song.count)
         Song.count += 1
+        print("After increment:", Song.count)
+    def add_to_genres(self):
+        Song.genres.append(self.genre)
+        #  Does not append genres to list
+
+
+
         
     
 
 out_of_touch = Song("Out of Touch", "Hall and Oates", "Pop")
 smells_like_teen_spirit = Song("Smells Like Teen Spirit", "Nirvana", "Rock")
+halo = Song("Halo", "Beyonce", "Pop")
+sara_smile = Song("Sara Smile", "Hall and Oates", "Pop")
+ninety_nine_problems = Song("99 Problems", "Jay-Z", "Rap")
 
 out_of_touch.__dict__
         
