af8cdc1a4556ab532e80f7e7d8ff247f3f6d08dc
James Hawk <jph94880@gmail.com>
2023-08-02 21:22:21 -0500

    Increments count += 1

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,28 @@
+#  Song Class:
+#   1. [] Show all artists of existing songs.
+#   2. [] Show all genres of existing songs.
+#   3. [] Track the number of songs of each genre created by the song class.
+#       a. Should return a dictionary with genre keys and int values.
+#   4. [] Shows the number of songs be each artist. 
+#       b. Returns a dictionary with artist keys and int values. 
+
+
+
 class Song:
-    pass
+    count = 0
+    def __init__(self, name, artist, genre):
+        self.name = name 
+        self.artist = artist 
+        self.genre = genre
+        self.add_song_to_count()
+    def add_song_to_count(self):
+        # Song objects are initializing. count is not incrementing += 1. 
+        Song.count += 1
+        
+    
+
+out_of_touch = Song("Out of Touch", "Hall and Oates", "Pop")
+smells_like_teen_spirit = Song("Smells Like Teen Spirit", "Nirvana", "Rock")
+
+out_of_touch.__dict__
+        
